bda4ec8ddd14dc4aa730f01cf1d4975978f36802
github username <github email address>
2020-05-26 22:26:10 -0400

    year and month populate

--- src/events.js
+++ src/events.js
@@ -1,23 +1,30 @@
 class Event {
-  constructor(name,start_date,end_date,location,journal_id){
-    this.name = name
-    this.start_date = start_date
-    this.end_date = end_date
-    this.location = location
+  constructor(params){
+    this.name = params.name
+    this.start_date = params.start_date
+    this.end_date = params.end_date
+    this.location = params.location
   }
 }
 
 
 // Get all events (as an array of instances) given a date range
-const getEvents = () => {
-  const allEvents = [];
-
-  fetch('http://localhost:3000/events')
+const getEvents = method => {
+  return fetch('http://localhost:3000/events')
   .then( res => res.json() )
-  .then( ev => {
-    ev.forEach(e => {
-      allEvents.push(new Event(e))
-    })
-  })
-  return allEvents;
-};
+  .then( events => {
+  events.forEach(event => {
+    let e = new Event({
+      name: event.name,
+      location: event.location,
+      start_date: new Date(Date.parse(event.start_date)),
+      end_date: new Date(Date.parse(event.end_date))
+    });
+    method(e)
+    });
+  });
+}
+
+const log = i => {
+  console.log(i)
+}
